f1919d38d7293e175d0562297ce5ac5c5dfe4716
chris <you@example.com>
2016-01-31 18:22:34 +0000

    run happens in gtk idle so it doesn't block the gui

---
+++ main.h
@@ -0,0 +1,22 @@
+
+#define AF1 context.R1.wr.AF
+#define BC1 context.R1.wr.BC
+#define DE1 context.R1.wr.DE
+#define HL1 context.R1.wr.HL
+#define AF2 context.R2.wr.AF
+#define BC2 context.R2.wr.BC
+#define DE2 context.R2.wr.DE
+#define HL2 context.R2.wr.HL
+#define IX context.R1.wr.IX
+#define IY context.R1.wr.IY
+#define SP context.R1.wr.SP
+#define PC context.PC
+#define I context.I
+#define R context.R
+#define IFF1 context.IFF1
+#define IFF2 context.IFF2
+#define IM context.IM
+
+
+static void dump_z80_state( void );
+
